122f23b77d322f351574285ee302117be76273d7
Viktor Andonov <allukard696@gmail.com>
2019-07-24 21:58:16 +0300

    Finished

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TheGeneral
+{
+    
+
+    public interface IOperationSuccessCalculator
+    {
+        bool IsAttackSuccesful(int x, int y);
+        bool IsDefenceSuccesful(int x, int y);
+    }
+
+    public class General : IOperationSuccessCalculator
+    {
+
+        public bool IsAttackSuccesful(int x, int y)
+        {
+            if (x > 200 || y > 300)
+            {
+                throw new System.Exception("Parameters exceed board dimensions!");
+            }
+
+            else if ((x + y - 5) % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool IsDefenceSuccesful(int x, int y)
+        {
+            if (x > 200 || y > 300)
+            {
+                throw new System.Exception("Parameters exceed board dimensions!");
+            }
+
+            else if ((x * y - 5) % 2 == 0)
+            {
+                return true;
+            }
+
+
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+
+}
---
+++ TheGeneral.UnitTests/Tets.cs
@@ -0,0 +1,84 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheGeneral.UnitTests
+{
+    [TestFixture]
+    public class Tets
+    {
+        General general;
+
+        [SetUp]
+        public void SetUp()
+        {
+            general = new General();
+        }
+
+        [Test]
+        [TestCase(10, 1)]
+        [TestCase(6, 1)]
+        [TestCase(-10, -1)]
+        [TestCase(0, 13)]
+        [TestCase(1, -2)]
+        public void Successfull_Attack_Inside_Board_Dimensions(int a, int b)
+        {
+            bool successfullAttack = general.IsAttackSuccesful(a, b);
+
+            Assert.That(successfullAttack, Is.True, "Attack was unsuccessfull!");
+        }
+
+        [Test]
+        [TestCase(11, 1)]
+        [TestCase(5, 1)]
+        [TestCase(-11, -1)]
+        [TestCase(-1, 13)]
+        [TestCase(0, -2)]
+        public void Unsucessfull_Attack_Outside_Board_Dimensions(int a, int b)
+        {
+            bool successfullAttack = general.IsAttackSuccesful(a, b);
+
+            Assert.That(successfullAttack, Is.False, "Attack was not supposed to be successfull!");
+        }
+
+        [Test]
+        [TestCase(11, 1)]
+        [TestCase(5, 1)]
+        [TestCase(-11, -1)]
+        [TestCase(-1, 13)]
+        [TestCase(-1, -21)]
+        public void Successfull_Defence_Inside_Board_Dimensions(int a, int b)
+        {
+            bool successfullDefence = general.IsDefenceSuccesful(a, b);
+
+            Assert.That(successfullDefence, Is.True, "Defence was unsuccessfull!");
+        }
+
+        [Test]
+        [TestCase(10, 1)]
+        [TestCase(6, 1)]
+        [TestCase(-10, -1)]
+        [TestCase(0, 13)]
+        [TestCase(1, -2)]
+        public void Unsucessfull_Defence_Outside_Board_Dimensions(int a, int b)
+        {
+            bool successfullDefence = general.IsDefenceSuccesful(a, b);
+
+            Assert.That(successfullDefence, Is.False, "Defence was not supposed to be successfull!");
+        }
+
+        [Test]
+        [TestCase(201, 299)]
+        [TestCase(200, 500)]
+        public void Invalid_Input_Outside_Board_Dimensions_Exception_Expected(int a, int b)
+        {
+
+            Exception ex = Assert.Throws<Exception>(() => general.IsDefenceSuccesful(a, b));
+
+        }
+        
+    }
+}
